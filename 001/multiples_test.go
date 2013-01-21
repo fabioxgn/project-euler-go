163e00fbbe6bf5ea14c967eeddcc57aa489481e8
Fabio Gomes <fabio.gomes@monde.com.br>
2013-01-21 13:14:55 -0200

    Removed solution to problem 001

--- 001/multiples_test.go
+++ 001/multiples_test.go
@@ -5,18 +5,9 @@ import (
 )
 
 func TestMultiplesOf3And5To10(t *testing.T) {
-	values := []int {3, 5}
+	values := []int{3, 5}
 	expected := 23
 	out := Sum(values, 10)
-	if out != expected {
-		t.Errorf("Sum(%v, %v) = %v, want %v", values[0], values[1], out, expected)
-	}
-}
-
-func TestMultiplesOf3And5To1000(t *testing.T) {
-	values := []int {3, 5}
-	expected := 233168
-	out := Sum(values, 1000)
 	if out != expected {
 		t.Errorf("Sum(%v, %v) = %v, want %v", values[0], values[1], out, expected)
 	}
